b441dcb6b0598f8e29a1e5a5571f9fb38c1ca12d
Diego Amicabile <diego.amicabile@gmail.com>
2017-05-02 18:30:58 +0200

    Created script to retrieve games from chessbase

---
+++ retrieve_from_chessbase.py
@@ -0,0 +1,80 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+ROOT_PAGE='http://en.chessbase.com'
+ROOT_POST_PAGE=ROOT_PAGE+'/post/'
+import traceback
+
+import argparse
+
+def iscbreplay_dataurl(tag):
+    return tag.has_attr('data-url')
+
+
+def iscbreplay_text(tag):
+
+    if tag.has_attr('class')  :
+        return tag.has_attr('class') and not tag.has_attr('data-url') and 'cbreplay' in tag.attrs['class']
+
+
+
+def get_pgnlinks_from_page(pageLink,debug=True):
+    pgnstrings =[]
+    print(pageLink)
+    r = requests.get(pageLink)
+    soup = BeautifulSoup(r.content)
+    if not soup:
+        return None
+
+    cbreplays_text = soup.find_all(iscbreplay_text)
+
+    cbreplays_dataurl = soup.find_all(iscbreplay_dataurl)
+    for cbreplay in cbreplays_dataurl :
+        pgnstrings.append( {"data-url":cbreplay.attrs['data-url']})
+
+    for cbreplay in cbreplays_text:
+        pgnstrings.append({"pgn-text": cbreplay.get_text()})
+
+    return pgnstrings
+
+def downoad_files(data_urls ):
+    for data_url in data_urls:
+        r =requests.get(ROOT_PAGE+data_url)
+        dfile = "from_cbase/"+ data_url.lower().replace('/','_').replace('\\','_').replace('_portals_all_','')
+        with open(dfile , "w",encoding='utf-8') as handle:
+            norlines = r.text.replace('\r\n','\n')
+            handle.write(norlines)
+
+def downoad_pgns(pgn_texts, page_link):
+    for index, pgn_text in enumerate(pgn_texts):
+        dfile = "from_cbase_raw/"+  page_link.replace(ROOT_POST_PAGE,'')+'_'+str(index)+'.pgn'
+        with open(dfile , "w",encoding='utf-8') as handle:
+            handle.write(pgn_text)
+
+
+def process_page(page_link):
+
+    page_link = page_link if ROOT_POST_PAGE in page_link else ROOT_POST_PAGE + page_link
+    pgnlinks = get_pgnlinks_from_page(page_link, debug=True)
+    print(pgnlinks)
+    data_urls = [x["data-url"] for x in pgnlinks if "data-url" in x]
+    downoad_files(data_urls)
+    pgn_texts = [x["pgn-text"] for x in pgnlinks if "pgn-text" in x]
+    downoad_pgns(pgn_texts, page_link)
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--fileLink')
+parser.add_argument('--debug')
+args = parser.parse_args()
+
+with open(args.fileLink, "r") as handle:
+    lines = handle.readlines()
+    for line in lines:
+        print("Processing %s" % line)
+        try:
+            process_page(line.strip())
+        except:
+            print("Could not process page %s " % line.strip())
+
+            traceback.print_exc()
