a3b35ba467c3d53e0c8337e8418673e5bf0a050f
Diego Amicabile <diego.amicabile@gmail.com>
2018-11-06 21:55:59 +0100

    on server

--- collect_links_from_chessbase.py
+++ collect_links_from_chessbase.py
@@ -7,7 +7,7 @@ import os
 import requests
 from bs4 import BeautifulSoup
 import re
-
+import sys
 def read_all_links(directory):
     all_links = []
     for filename in os.listdir(directory):
@@ -16,7 +16,7 @@ def read_all_links(directory):
             print("Processing {}".format(total_path))
             with open(total_path , 'r') as file_handle:
                 all_lines = file_handle.readlines()
-                all_links += all_lines
+                all_links += [line.replace('http://', 'https://').rstrip() for line in all_lines]
     return all_links
 
 def retrieve_posts_page(pageLink):
@@ -39,7 +39,32 @@ def convert_tag(tag_link):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--debug')
+    parser.add_argument('--out')
+
     args = parser.parse_args()
+    if (args.out) and os.path.isfile('chessbase_links/'+args.out):
+        print('{} file exists...Exiting'.format(args.out))
+        exit(0)
     read_links = read_all_links('chessbase_links')
-    all_posts = retrieve_posts_page('https://en.chessbase.com/0')
-    print(all_posts)
+    page_num = 0
+    found = False
+    to_parse_links = []
+    while not found:
+        link_to_parse = 'https://en.chessbase.com/{}'.format(page_num)
+        print('Examining {}'.format(link_to_parse))
+        all_posts = retrieve_posts_page(link_to_parse)
+        for post in all_posts:
+            if post in read_links:
+                found = True
+                print('Found {} in history'.format(post))
+            else:
+                to_parse_links.append(post)
+                print(post)
+        page_num += 1
+    if args.out:
+        fout = open('chessbase_links/'+args.out,'a')
+    else:
+        fout = sys.stdout
+    for link in to_parse_links:
+        print(link, file=fout)
+    fout.close()
